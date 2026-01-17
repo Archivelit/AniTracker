227309161c59fea0c5341cf61bf40a3f8daccac7
Archivelit <dalbaeb1488322666@gmail.com>
2026-01-17 15:15:29 +0100

    Create working hello for user with hardcoded preregistred email

---
+++ src/AniTracker.Frontend/src/Models/user.ts
@@ -0,0 +1,11 @@
+export default class User {
+    id: string;
+    readonly username: string;
+    readonly email: string;
+
+    constructor(id: string, username: string, email: string) {
+        this.id = id
+        this.username = username
+        this.email = email 
+    }
+}
--- src/AniTracker.Frontend/src/app/page.tsx
+++ src/AniTracker.Frontend/src/app/page.tsx
@@ -1,8 +1,31 @@
+import { Spinner } from "@/components/ui/spinner";
+import User from "@/Models/user";
+import { api } from "@/utils/Api";
 import type { ReactNode } from "react";
 
 export default async function Home(): Promise<ReactNode> {
-  return (
-    <>
-    </>
+  const response = await api.get<User>(
+    "/users",
+    { params: { email: "example@domain.com" } }
   );
+
+  const user = response.data;
+
+  return (
+    <div>
+      <MainContent user={user} />
+    </div>
+  );
+}
+
+function MainContent({ user }: MainContentArgs): ReactNode {
+  return (
+    <div>
+      <h1>Hello, {user.username}!</h1>
+    </div>
+  );
+}
+
+interface MainContentArgs {
+  user: User;
 }
