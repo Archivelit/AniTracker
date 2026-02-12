314d44a7f5a78214c2303b304961053aa3be64bc
Archivelit <vladyslavshumilo@gmail.com>
2026-02-12 09:26:22 +0100

    Update migrations

--- src/AniTracker.Api/Migrations/20260212082540_Add user roles.Designer.cs
+++ src/AniTracker.Api/Migrations/20260212082540_Add user roles.Designer.cs
@@ -4,6 +4,7 @@ using AniTracker.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AniTracker.Api.Migrations
 {
     [DbContext(typeof(AniTrackerDbContext))]
-    partial class AniTrackerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260212082540_Add user roles")]
+    partial class Adduserroles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
